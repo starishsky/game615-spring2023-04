a7b5b6fad0fc3ee6dfb9e527a656165f25494e0a
starishsky <117216668+starishsky@users.noreply.github.com>
2023-02-14 03:20:41 -0500

    you know

--- exercise04/Assets/PlaneScript.cs
+++ exercise04/Assets/PlaneScript.cs
@@ -4,19 +4,64 @@ using UnityEngine;
 
 public class PlaneScript : MonoBehaviour
 {
-    float forwardSpeed = 50;
-    float rotateSpeed = 20;
+    float forwardSpeed = 5.0f;
+    float rotateSpeed = 0.1f;
+    float rotationResetSpeed = 0.1f;
+    Quaternion InitialRotation;
+    Vector3 InitialPosition;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //InitialPosition = gameObject.transform.position;
+        InitialRotation = gameObject.transform.rotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Rotate(0, rotateSpeed * Time.deltaTime, 0, Space.Self);
+        //gameObject.transform.Rotate(0, rotateSpeed * Time.deltaTime, 0, Space.Self);
         gameObject.transform.Translate(transform.forward * forwardSpeed * Time.deltaTime, Space.World);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(Vector3.up * 50f * Time.deltaTime, Space.World);
+            gameObject.transform.Rotate(-rotateSpeed,0,0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, InitialRotation, Time.deltaTime * rotationResetSpeed);
+            //gameObject.transform.Translate(transform.forward * forwardSpeed * Time.deltaTime, Space.World);
+         
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(Vector3.left * 50f * Time.deltaTime, Space.World);
+            gameObject.transform.Rotate(0, -rotateSpeed, 0);
+        }
+
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(Vector3.down * 30f * Time.deltaTime, Space.World);
+            gameObject.transform.Rotate(rotateSpeed, 0, 0);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, InitialRotation, Time.deltaTime * rotationResetSpeed) ;
+            //gameObject.transform.Translate(transform.forward * forwardSpeed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(Vector3.right * 50f * Time.deltaTime, Space.World);
+            gameObject.transform.Rotate(0, rotateSpeed, 0);
+        }
+
     }
+
 }
---
+++ exercise04/Assets/knifeScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knifeScript : MonoBehaviour
+{
+    public GameObject sharpKnife;
+    public GameObject pinkBalloon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                GameObject knife = (GameObject)Instantiate(sharpKnife, transform.position, transform.rotation);
+                float rotXAmount = Random.Range(-3, 0);
+                float rotYAmount = 0;
+                knife.transform.Rotate(rotXAmount, rotYAmount, 0);
+                Rigidbody rb = knife.GetComponent<Rigidbody>();
+                rb.AddForce(knife.transform.forward * 1000);
+
+                Destroy(knife, 2f);
+            }
+        }
+       
+            
+       
+        
+    }
+}
